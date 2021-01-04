d4aadcba08f2cc34fa627e80025da83a3268c42c
Amos Chang <amos.chang@bigtera.com>
2021-01-04 22:39:19 +0800

    ch4: implement main package of cache-benchmark

--- src/cache_benchmark/main.go
+++ src/cache_benchmark/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"math/rand"
+	"strings"
 	"time"
 )
 
@@ -32,5 +33,116 @@ func init() {
 }
 
 func main() {
+	ch := make(chan *result, threads)
+	res := &result{0, 0, 0, make([]statistic, 0)}
+	start := time.Now()
+	for i := 0; i < threads; i++ {
+		go operate(i, total/threads, ch)
+	}
+	for i := 0; i < threads; i++ {
+		res.addResult(<-ch)
+	}
+	d := time.Now().Sub(start)
+	totalCount := res.getCount + res.missCount + res.setCount
+	fmt.Printf("%d records get\n", res.getCount)
+	fmt.Printf("%d records miss\n", res.missCount)
+	fmt.Printf("%d records set\n", res.setCount)
+	fmt.Printf("%d records total\n", totalCount)
+	fmt.Printf("%f seconds total\n", d.Seconds)
+	statCountSum := 0
+	statTimeSum := time.Duration(0)
+	for b, s := range res.statBuckets {
+		if s.count == 0 {
+			continue
+		}
+		statCountSum += s.count
+		statTimeSum += s.time
+		fmt.Printf("%d%% requests < %d ms\n", statCountSum*100/totalCount, b+1)
+	}
+	fmt.Printf("%d usec average for each request\n", int64(statTimeSum/time.Microsecond)/int64(statCountSum))
+	fmt.Printf("throughput is %f MB/s\n", float64((res.getCount+res.setCount)*valueSize)/1e6/d.Seconds())
+	fmt.Printf("rps is %f\n", float64(totalCount)/float64(d.Seconds()))
+}
 
+func operate(id, count int, ch chan *result) {
+	client := cacheClient.New(typ, server)
+	cmds := make([]*cacheClient.Cmd, 0)
+	valuePrefix := strings.Repeat("a", valueSize)
+	r := &result{0, 0, 0, make([]statistic, 0)}
+	for i := 0; i < count; i++ {
+		var tmp int
+		if keyspacelen > 0 {
+			tmp = rand.Intn(keyspacelen)
+		} else {
+			tmp = id*count + 1
+		}
+		key := fmt.Sprintf("%d", tmp)
+		value := fmt.Sprintf("%s%d", valuePrefix, tmp)
+		name := operation
+		if operation == "mixed" {
+			if rand.Intn(2) == 1 {
+				name = "set"
+			} else {
+				name = "get"
+			}
+		}
+		c := cacheClient.cmd{name, key, value, nil}
+		if pipelen > 1 {
+			cmds = append(cmds, c)
+			if len(cmds) == pipelen {
+				pipeline(client, cmds, r)
+				cmds = make([]*cacheClient.Cmd, 0)
+			}
+		} else {
+			run(client, c, r)
+		}
+	}
+	if len(cmds) != 0 {
+		pipeline(client, cmds, r)
+	}
+	ch <- r
+}
+
+type statistic struct {
+	count int
+	time  time.Duration
+}
+type result struct {
+	getCount    int
+	missCount   int
+	setCount    int
+	statBuckets []statistic
+}
+
+func (r *result) addStatistic(bucket int, stat statistic) {
+	if bucket > len(r.statBuckets)-1 {
+		newStatBuckets := make([]statistic, bucket+1)
+		copy(newStatBuckets, r.statBuckets)
+		r.statBuckets = newStatBuckets
+	}
+	s := r.statBuckets[bucket]
+	s.count += stat.count
+	s.time += stat.time
+	r.statBuckets[bucket] = s
+}
+
+func (r *result) addDuration(d time.Duration, typ string) {
+	bucket := int(d / time.Millisecond)
+	r.addStatistic(bucket, statistic{1, d})
+	if typ == "get" {
+		r.getCount++
+	} else if typ == "set" {
+		r.setCount++
+	} else {
+		r.missCount++
+	}
+}
+
+func (r *result) addResult(src *result) {
+	for b, s := range src.statBuckets {
+		r.addStatistic(b, s)
+	}
+	r.getCount += src.getCount
+	r.missCount += src.missCount
+	r.setCount += src.setCount
 }
