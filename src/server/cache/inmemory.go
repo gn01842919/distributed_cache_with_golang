5cc0ea07f545076e3c319c0a8db207112fc0f29c
Amos Chang <amos.chang@bigtera.com>
2021-01-10 13:41:18 +0800

    ch5: implement flushBatch for http client (not in book)

--- src/server/cache/inmemory.go
+++ src/server/cache/inmemory.go
@@ -2,24 +2,71 @@ package cache
 
 import (
 	"sync"
+	"time"
 )
 
 type inMemoryCache struct {
 	c     map[string][]byte
 	mutex sync.RWMutex
 	Stat  // TODO: Why not pointer here but uses pointer in cacheHandler in http/cache.go??
+	ch    chan *pair
+}
+
+type pair struct {
+	k string
+	v []byte
 }
 
 func (c *inMemoryCache) Set(k string, v []byte) error {
+	c.ch <- &pair{k, v}
+	return nil
+}
+
+// BatchSize : the size a batch of k-v pairs to write to cache at once
+const BatchSize = 100
+
+func (c *inMemoryCache) flushBatch(keys [BatchSize]string, vals [BatchSize][]byte, count int) {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
-	tmp, exist := c.c[k]
-	if exist {
-		c.del(k, tmp) // Seems to make Stat correct
+	for i := 0; i < count; i++ {
+		k := keys[i]
+		v := vals[i]
+		tmp, exist := c.c[k]
+		if exist {
+			c.del(k, tmp)
+		}
+		c.c[k] = v
+		c.add(k, v) // add stat
 	}
-	c.c[k] = v
-	c.add(k, v)
-	return nil
+}
+
+func (c *inMemoryCache) writeFunc(ch chan *pair) {
+	count := 0
+	t := time.NewTimer(time.Second)
+	var keys [BatchSize]string
+	var vals [BatchSize][]byte
+	for {
+		select {
+		case p := <-ch:
+			keys[count] = p.k
+			vals[count] = p.v
+			count++
+			if count == BatchSize {
+				c.flushBatch(keys, vals, count)
+				count = 0
+			}
+			if !t.Stop() { // t.Stop() returns false when the timer has been triggered
+				<-t.C // remove an item for timer's channel to avoid going the the second case at once
+			}
+			t.Reset(time.Second)
+		case <-t.C:
+			if count != 0 {
+				c.flushBatch(keys, vals, count)
+				count = 0
+			}
+			t.Reset(time.Second)
+		}
+	}
 }
 
 func (c *inMemoryCache) Del(k string) error {
@@ -46,6 +93,9 @@ func (c *inMemoryCache) GetStat() Stat {
 func newInMemoryCache() *inMemoryCache {
 	// Solved: So we can return a reference to a local variable?
 	// ==> Yes. In go, this will be saved in heap. It is same as using new()
-	return &inMemoryCache{
-		make(map[string][]byte), sync.RWMutex{}, Stat{}}
+	ch := make(chan *pair, 5000)
+	c := &inMemoryCache{
+		make(map[string][]byte), sync.RWMutex{}, Stat{}, ch}
+	go c.writeFunc(ch)
+	return c
 }
